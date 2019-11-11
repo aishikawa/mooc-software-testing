8464a2c90b47ceb769185cb00e479e10cd6de386
A Ishikawa <a.ishikawa3@gmail.com>
2019-11-11 23:00:55 +0900

    Add test for CountLetters

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -17,4 +17,9 @@ public class CountLettersTest {
         Assertions.assertEquals(1, words);
     }
 
+    @Test
+    public void endsWithR() {
+        int words = new CountLetters().count("cat|our|car");
+        Assertions.assertEquals(2, words);
+    }
 }
