0fc4aa2490154e9e7666b8280f8c494c858d1766
A Ishikawa <a.ishikawa3@gmail.com>
2019-11-13 23:50:42 +0900

    Write test for CaesarShiftCipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,32 @@
 package tudelft.caesarshift;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
+    @Test
+    public void positiveShift() {
+        CaesarShiftCipher csh = new CaesarShiftCipher();
+        Assertions.assertEquals("def", csh.CaesarShiftCipher("abc", 3));
+    }
+
+    @Test
+    public void negativeShift() {
+        CaesarShiftCipher csh = new CaesarShiftCipher();
+        Assertions.assertEquals("xyz", csh.CaesarShiftCipher("abc", -3));
+    }
+
+    @Test
+    public void invalidMessage() {
+        CaesarShiftCipher csh = new CaesarShiftCipher();
+        Assertions.assertEquals("invalid", csh.CaesarShiftCipher("Abc", 1));
+    }
+
+    @Test
+    public void messageWithSpace() {
+        CaesarShiftCipher csh = new CaesarShiftCipher();
+        Assertions.assertEquals("bcd yza", csh.CaesarShiftCipher("abc xyz", 1));
+    }
 
 }
